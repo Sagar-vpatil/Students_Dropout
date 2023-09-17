6ab41f4904d70f7fccc8c2d123a59214aeb308f3
SVP2408 <125073576+SVP2408@users.noreply.github.com>
2023-09-17 22:47:30 +0530

    admin panel customize

--- Students_Dropout/settings.py
+++ Students_Dropout/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- myapp/admin.py
+++ myapp/admin.py
@@ -5,6 +5,9 @@ from .models import StudentsInfo
 @admin.register(StudentsInfo)
 class StudentsAdmin(ImportExportModelAdmin):
     list_display = ('Student_name','Standard','Aadhaar_no')
+    list_per_page = 30
+    search_fields = ('Student_name',)
+    list_filter = ('Gender',)
     
 #admin.site.register(StudentsInfo)
 
